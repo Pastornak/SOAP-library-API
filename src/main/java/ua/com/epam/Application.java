06e99ea1014f44e61115345fd8d0811b31ce33d1
Pastornak <yurapaster@gmail.com>
2020-04-12 17:38:24 +0300

    Create DataIngestion class which generates DB data and call it on service start-up

--- src/main/java/ua/com/epam/Application.java
+++ src/main/java/ua/com/epam/Application.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        try {
+            DataIngestion.main(args);
+        } catch(Exception e) {
+            System.err.println("Failed generating ingestion data");
+            e.printStackTrace();
+        }
         SpringApplication.run(Application.class, args);
     }
 }
